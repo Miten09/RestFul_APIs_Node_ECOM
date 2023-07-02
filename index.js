2bcd0d8e4c75dbea0b8f42340928edc38eecb6a8
Miten09 <124608667+Miten09@users.noreply.github.com>
2023-07-02 20:01:18 +0530

    RestAPI Forgot & Reset Password Done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://Miten:MiHaKrRa88@projects.hpzcfyg.mongodb.net/ecommerce?retryWrites=true&w=majority"
+    "mongodb+srv://Miten:@projects.hpzcfyg.mongodb.net/ecommerce?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("MongoDB connection established");
